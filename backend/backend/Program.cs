2c9b9c5a80118cfa40938cfdbc97448f93ecc7b5
Ryan Deist <ryandeist@gmail.com>
2025-08-13 14:47:05 -0400

    Set up default connection string and registered AppDbContext in Program.cs.

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,7 +1,13 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // services
 builder.Services.AddControllers();
+
+string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentNullException("connectionString is null.");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
