bfc15ec45e0a21131583c4a234fe22bcdbf8e74c
Ryan Deist <ryandeist@gmail.com>
2025-08-13 15:29:19 -0400

    AddPerson controller built.

--- backend/backend/Controllers/PeopleController.cs
+++ backend/backend/Controllers/PeopleController.cs
@@ -1,3 +1,4 @@
+using backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -6,4 +7,26 @@ namespace backend.Controllers;
 [ApiController]
 public class PersonController : ControllerBase
 {
+    private readonly AppDbContext _context;
+
+    public PersonController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPerson(Person person)
+    {
+        try
+        {
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+            return Ok(person); // return 200 status code with the created person object in the response body.
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // return 500 status code with the error message in the response body.
+        }
+    }
 }
