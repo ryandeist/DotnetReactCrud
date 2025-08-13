e96d346f5cdf45f795892f541c57e20c8fdeed2d
Ryan Deist <ryandeist@gmail.com>
2025-08-13 16:02:27 -0400

    GET all request built and  tested. Working.

--- backend/backend/Controllers/PeopleController.cs
+++ backend/backend/Controllers/PeopleController.cs
@@ -1,5 +1,6 @@
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
@@ -23,6 +24,20 @@ public class PeopleController : ControllerBase
             await _context.SaveChangesAsync();
             return Ok(person); // return 200 status code with the created person object in the response body.
 
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // return 500 status code with the error message in the response body.
+        }
+    }
+
+    [HttpGet] // GET /api/people
+    public async Task<IActionResult> GetPeople()
+    {
+        try
+        {
+            var people = await _context.People.ToListAsync();
+            return Ok(people); // return 200 status code with the list of people in the response body.
         }
         catch (Exception ex)
         {
