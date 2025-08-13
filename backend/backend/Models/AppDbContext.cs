a8ce4a070ca7462170d87874cc79b555d690d286
Ryan Deist <ryandeist@gmail.com>
2025-08-13 14:37:39 -0400

    Scaffold AppDbContext.

--- backend/backend/Models/AppDbContext.cs
+++ backend/backend/Models/AppDbContext.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace backend.Models;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
 
+    }
+
+    public DbSet<Person> People { get; set; }
 }
