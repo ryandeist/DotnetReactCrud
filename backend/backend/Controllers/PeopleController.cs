7ecbfb8fefda05b1ed0572593a4d829b3c119725
Ryan Deist <ryandeist@gmail.com>
2025-08-13 14:25:49 -0400

    Person Model created.

---
+++ backend/backend/Controllers/PeopleController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// services
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// middleware
+app.MapControllers();
 
 app.Run();
