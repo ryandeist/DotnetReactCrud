1f462438735239d934b88d2fd1b76a5eefb93c76
Ryan Deist <ryandeist@gmail.com>
2025-08-13 20:44:41 -0400

    PUT person method created and tested.

--- backend/backend/Controllers/PeopleController.cs
+++ backend/backend/Controllers/PeopleController.cs
@@ -23,7 +23,7 @@ public class PeopleController : ControllerBase
         {
             _context.People.Add(person);
             await _context.SaveChangesAsync();
-            return CreatedAtRoute("GetPerson", new {id=person.PersonId}, person); // return 201 status + the location of the resource code with the created person object in the response body.
+            return CreatedAtRoute("GetPerson", new { id = person.PersonId }, person); // return 201 status + the location of the resource code with the created person object in the response body.
 
         }
         catch (Exception ex)
@@ -58,6 +58,31 @@ public class PeopleController : ControllerBase
                 return NotFound(); // return 404 status code if the person with the given id is not found.
             }
             return Ok(person); // return 200 status code with the list of people in the response body.
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); // return 500 status code with the error message in the response body.
+        }
+    }
+
+    [HttpPut("{id:int}")] // PUT /api/people/{id}
+    public async Task<IActionResult> UpdatePerson(int id,[FromBody] Person person)
+    {
+        try
+        {
+            if (id != person.PersonId)
+            {
+                return BadRequest("Person ID mismatch"); // return 400 status code if the id in the URL does not match the id in the person object.
+            }
+
+            if (!await _context.People.AnyAsync(person => person.PersonId == id))
+            {
+                return NotFound(); // return 404 status code if the person with the given id is not found.
+            }
+
+            _context.People.Update(person);
+            await _context.SaveChangesAsync();
+            return NoContent(); // return 204 status code with no content in the response body.
         }
         catch (Exception ex)
         {
